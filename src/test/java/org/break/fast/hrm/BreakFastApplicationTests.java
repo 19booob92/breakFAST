b072d9da0d9e0a42a88c153b4f6ef2713d2603f8
Mateusz <19mateusz92@gmail.com>
2016-06-12 15:46:35 +0200

    init commit

---
+++ src/test/java/org/break/fast/hrm/BreakFastApplicationTests.java
@@ -0,0 +1,18 @@
+package org.break.fast.hrm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BreakFastApplication.class)
+@WebAppConfiguration
+public class BreakFastApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
